78904fd6c720a7a7aa4454f8e9217df00cc58736
IvanKurnakov <ivankurnakov@yandex.ru>
2016-01-27 12:14:27 +0500

    versoin 0.9
    stable version. few minor bugs with anim layer on some devices

---
+++ app/src/main/java/ru/kurnakovik/simple2048/Config.java
@@ -0,0 +1,10 @@
+package ru.kurnakovik.simple2048;
+
+/**
+ * Created by Skinner2170 on 05.01.2016.
+ */
+public class Config {
+
+    public static final int LINES = 4;
+    public static int CARD_WIDTH = 0;
+}
